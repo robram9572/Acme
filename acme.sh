f9f296562df8f13fefb7562b1e7f4e7ae6fcd3bf
robram9572 <34120952+robram9572@users.noreply.github.com>
2022-12-29 21:04:22 +0800

    Update acme.sh

--- acme.sh
+++ acme.sh
@@ -20,6 +20,6 @@ curl https://get.acme.sh | sh -s email=my@example.com
 source ~/.bashrc
 
 申请证书
-~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+#~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
 ~/.acme.sh/acme.sh --issue -d "$domainName" --standalone -k ec-256
 ~/.acme.sh/acme.sh --installcert -d "$domainName" --key-file /root/private.key --fullchain-file /root/cert.crt --ecc
